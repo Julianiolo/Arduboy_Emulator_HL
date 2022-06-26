54f0f2e097b5549b824210363d29a773551fd8fc
Julianiolo <50519317+Julianiolo@users.noreply.github.com>
2022-06-26 02:27:35 +0200

    formatting changes

--- src/Arduboy.h
+++ src/Arduboy.h
@@ -20,20 +20,21 @@ public:
 
 	uint8_t buttonState = 0;
 	enum {
-		Button_Up_Bit =    7,
+		Button_Up_Bit    = 7,
 		Button_Right_Bit = 6,
-		Button_Left_Bit =  5,
-		Button_Down_Bit =  4,
-		Button_A_Bit =     3,
-		Button_B_Bit =     2
+		Button_Left_Bit  = 5,
+		Button_Down_Bit  = 4,
+		Button_A_Bit     = 3,
+		Button_B_Bit     = 2
 	};
 	enum {
-		Button_Up =    1 << Button_Up_Bit,
+		Button_None  = 0,
+		Button_Up    = 1 << Button_Up_Bit,
 		Button_Right = 1 << Button_Right_Bit,
-		Button_Left =  1 << Button_Left_Bit,
-		Button_Down =  1 << Button_Down_Bit,
-		Button_A =     1 << Button_A_Bit,
-		Button_B =     1 << Button_B_Bit
+		Button_Left  = 1 << Button_Left_Bit,
+		Button_Down  = 1 << Button_Down_Bit,
+		Button_A     = 1 << Button_A_Bit,
+		Button_B     = 1 << Button_B_Bit
 	};
 
 	Arduboy();
